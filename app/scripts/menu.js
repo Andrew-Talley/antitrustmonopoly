c67ea30cc1b62918f630a7ae088119c4655d697f
Andrew Talley <drew.talley0@gmail.com>
2017-03-26 17:45:45 -0600

    Next part of test

--- app/scripts/menu.js
+++ app/scripts/menu.js
@@ -1,6 +1,6 @@
 var app = angular.module('menuApp', []);
 app.controller('menuController', function ($scope, $http) {
-    var allData = $http.get('game-types.json').success(function (response) {
+    var allData = $http.get('http://www.antitrustmonopoly.com/game-types.json').success(function (response) {
         $scope.allOptions = JSON.parse(response);
     })
 })
