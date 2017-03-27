8cf342c8845b612336dbbcbb2c7a9be61d1864b0
Andrew Talley <drew.talley0@gmail.com>
2017-03-27 12:40:00 -0600

    Updating final check for the menu

--- app/scripts/menu.js
+++ app/scripts/menu.js
@@ -1,9 +1,28 @@
 var app = angular.module('menuApp', []);
 app.controller('menuController', function ($scope, $http) {
+    $scope.allOptions = [];
+    $scope.groups = [];
     $http.get('https://www.antitrustmonopoly.com/app/scripts/game-types.json').success(function (response) {
         $scope.allOptions = response;
     });
     $http.get('https://www.antitrustmonopoly.com/app/scripts/monopoly-names.json').success(function (response) {
         $scope.groups = response;
-    })
+    });
+    $scope.addProp = function (index) {
+        $scope.groups[index].properties.push({ "name": "", "value": 0 });
+    }
+    $scope.addPropGroup = function () {
+        $scope.groups.push({
+            "bgColor": "#FF0000",
+            "properties": [
+                {
+                    "name": "",
+                    "value": 0
+                }
+            ]
+        })
+    }
+    $scope.leftClicked = function (i) {
+        $(i.target).parent().children('input').click();
+    }
 });
