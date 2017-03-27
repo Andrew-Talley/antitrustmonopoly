64f0b59dc799425bf218a4e26c77f458a685339b
Andrew Talley <drew.talley0@gmail.com>
2017-03-26 19:10:37 -0600

    Updated names, menu

--- app/scripts/menu.js
+++ app/scripts/menu.js
@@ -1,9 +1,9 @@
 var app = angular.module('menuApp', []);
 app.controller('menuController', function ($scope, $http) {
-    $http.get('http://www.antitrustmonopoly.com/app/scripts/game-types.json').success(function (response) {
+    $http.get('https://www.antitrustmonopoly.com/app/scripts/game-types.json').success(function (response) {
         $scope.allOptions = response;
     });
-    $http.get('http://www.antitrustmonopoly.com/app/scripts/monopoly-names.json').success(function (response) {
+    $http.get('https://www.antitrustmonopoly.com/app/scripts/monopoly-names.json').success(function (response) {
         $scope.groups = response;
     })
 });
