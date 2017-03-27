0439aeeff38b7bd0f3c15f8bb650377dc1ecf82c
Andrew Talley <drew.talley0@gmail.com>
2017-03-26 18:36:11 -0600

    Starting ability to change properties

--- app/scripts/menu.js
+++ app/scripts/menu.js
@@ -1,6 +1,9 @@
 var app = angular.module('menuApp', []);
 app.controller('menuController', function ($scope, $http) {
-    var allData = $http.get('http://www.antitrustmonopoly.com/app/scripts/game-types.json').success(function (response) {
+    $http.get('http://www.antitrustmonopoly.com/app/scripts/game-types.json').success(function (response) {
         $scope.allOptions = response;
+    });
+    $http.get('http://www.antitrustmonopoly.com/app/scripts/monopoly-names.json').success(function (response) {
+        $scope.groups = response;
     })
-})
+});
